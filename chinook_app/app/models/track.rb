4aba5d773cfcef00433de4ba18d4e3c32b7701ac
vrunda2712 <angy.v27@gmail.com>
2017-06-13 10:42:14 -0400

    Change class methods

--- chinook_app/app/models/track.rb
+++ chinook_app/app/models/track.rb
@@ -3,20 +3,24 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG = 360000
 
-  def self.short
-    # where('milliseconds < ?', SHORT)
-    shorter_than(SHORT)
-  end
+  scope :starts_with_scope, -> (char) { where('name ILIKE ?', "#{char}%") }
+  # def self.short
+  #   # where('milliseconds < ?', SHORT)
+  #   shorter_than(SHORT)
+  # end
+  scope :short, -> {shorter_than(SHORT)}
 
-  def self.medium
-    # where('milliseconds >= ? AND milliseconds < ?', SHORT, LONG)
-    longer_than(SHORT).shorter_than(LONG)
-  end
+  # def self.medium
+  #   # where('milliseconds >= ? AND milliseconds < ?', SHORT, LONG)
+  #   longer_than(SHORT).shorter_than(LONG)
+  # end
+  scope :medium, -> {longer_than(SHORT).shorter_than(LONG)}
 
-  def self.long
-    # where('milliseconds >= ?', LONG)
-    longer_than(LONG)
-  end
+  # def self.long
+  #   # where('milliseconds >= ?', LONG)
+  #   longer_than(LONG)
+  # end
+  scope :long, -> {longer_than(LONG)}
 
   def self.shorter_than(milliseconds)
     where('milliseconds < ?', milliseconds)
@@ -25,4 +29,12 @@ class Track < ApplicationRecord
   def self.longer_than(milliseconds)
     where('milliseconds >= ?', milliseconds)
   end
+
+  # def self.starts_with_method(char)
+  #   if char.present
+  #     { where('name ILIKE ?', "#{char}%") }
+  #   else
+  #     none
+  #   end
+  # end
 end
