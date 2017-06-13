8e785e0895a9b093efccc9c5741dd43170bf6157
vrunda2712 <angy.v27@gmail.com>
2017-06-13 10:26:20 -0400

    Add scope class methods for instances

--- chinook_app/app/models/track.rb
+++ chinook_app/app/models/track.rb
@@ -1,2 +1,28 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG = 360000
+
+  def self.short
+    # where('milliseconds < ?', SHORT)
+    shorter_than(SHORT)
+  end
+
+  def self.medium
+    # where('milliseconds >= ? AND milliseconds < ?', SHORT, LONG)
+    longer_than(SHORT).shorter_than(LONG)
+  end
+
+  def self.long
+    # where('milliseconds >= ?', LONG)
+    longer_than(LONG)
+  end
+
+  def self.shorter_than(milliseconds)
+    where('milliseconds < ?', milliseconds)
+  end
+
+  def self.longer_than(milliseconds)
+    where('milliseconds >= ?', milliseconds)
+  end
 end
